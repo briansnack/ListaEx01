b466e7f85acf3186e6637a94f0dfe3a064ed59e6
Brian <snackbrian10@gmail.com>
2024-03-14 01:04:05 -0300

    commit Ex05

--- Ex05.java
+++ Ex05.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Ex05 {
     public static void executar(){
-        Scanner leitor = 
+        Scanner leitor = new Scanner(System.in);{
+            System.out.println("Digite o primeiro número");
+            int numA = leitor.nextInt();
+
+            System.out.println("Digite o segundo número");
+            int numB = leitor.nextInt();
+
+            int aux = numA;
+            numA = numB;
+            numB = aux;
+
+            System.out.println("Valor original primeiro número: " + numB);
+            System.out.println("Valor original segundo número: " + numA);
+            System.out.println("");
+            System.out.println("Valor alterado do primeiro número: " + numA);
+            System.out.println("Valor alterado do segundo número: " + numB);
+        }
+        leitor.close();
     }
 }
