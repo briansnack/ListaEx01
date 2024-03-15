cc8d41cf80735fa32e066dfc3a6abc37bcb8953f
Brian <snackbrian10@gmail.com>
2024-03-14 22:59:46 -0300

    commit Ex08

---
+++ Ex08.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex08 {
+    public static void executar(){
+        Scanner leitor = new Scanner(System.in);{
+            System.out.println("Digite um número: ");
+            int num = leitor.nextInt();
+
+            if (num > 50) {
+                System.out.println("Número maior que 50");
+            } else{
+                System.out.println("Número menor que 50");
+            }
+
+
+
+        leitor.close();
+        }
+    }
+}
--- Programa.java
+++ Programa.java
@@ -6,6 +6,20 @@ public class Programa {
         // Ex04.executar();
         // Ex05.executar();
         // Ex06.executar();
-        Ex07.executar();
+        // Ex07.executar();
+        Ex08.executar();
+        // Ex09.executar();
+        // Ex10.executar();
+        // Ex11.executar();
+        // Ex12.executar();
+        // Ex13.executar();
+        // Ex14.executar();
+        // Ex15.executar();
+        // Ex16.executar();
+        // Ex17.executar();
+        // Ex18.executar();
+        // Ex19.executar();
+        // Ex20.executar();
+
     }
 }
