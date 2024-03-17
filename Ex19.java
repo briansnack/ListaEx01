472e25c4b4798791f13053eadfb4c44472fdbe79
Brian <snackbrian10@gmail.com>
2024-03-17 10:19:56 -0300

    commit Ex19

---
+++ Ex19.java
@@ -0,0 +1,9 @@
+public class Ex19 {
+    public static void executar(){
+        double raio = 10;
+        double altura = 15;
+        double volume = 3.14 * (raio * raio) * altura;
+
+        System.out.println("Volume do cilindro: " + volume);
+    }
+}
